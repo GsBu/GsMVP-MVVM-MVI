65a9de2977124d99ed30e9c5c66a6d9871c217e9
Gs <Gs_Android@163.com>
2020-01-15 16:23:30 +0800

    优化代码

--- app/src/main/java/com/example/gs/gsmvp/search/SearchActivity.java
+++ app/src/main/java/com/example/gs/gsmvp/search/SearchActivity.java
@@ -3,8 +3,6 @@ package com.example.gs.gsmvp.search;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.example.gs.gsmvp.R;
@@ -13,21 +11,7 @@ import com.example.gs.gsmvp.base.BaseMVPActivity;
 public class SearchActivity extends BaseMVPActivity<SearchContract.View, SearchContract.Presenter>
         implements SearchContract.View{
 
-    FloatingActionButton fab;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
-
-        fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mPresenter.sendEmail();
-            }
-        });
-    }
+    FloatingActionButton fabEmail;
 
     @Override
     protected void onInitParams(Bundle bundle) {
@@ -36,7 +20,15 @@ public class SearchActivity extends BaseMVPActivity<SearchContract.View, SearchC
 
     @Override
     protected void setupViews(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_search);
 
+        fabEmail = findViewById(R.id.fab_email);
+        fabEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mPresenter.sendEmail();
+            }
+        });
     }
 
     @Override
@@ -46,7 +38,7 @@ public class SearchActivity extends BaseMVPActivity<SearchContract.View, SearchC
 
     @Override
     public void sendSuccess(String str) {
-        Snackbar.make(fab, str, Snackbar.LENGTH_LONG)
+        Snackbar.make(fabEmail, str, Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show();
     }
 }
