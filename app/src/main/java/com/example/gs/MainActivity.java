0b4796e69eb1e1b70d375af2e14e8dccad4011b7
GsBu <1129652584@qq.com>
2022-06-15 13:28:58 +0800

    重新组织页面结构2

--- app/src/main/java/com/example/gs/MainActivity.java
+++ app/src/main/java/com/example/gs/MainActivity.java
@@ -8,11 +8,12 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.gs.gsmvp.home.HomeActivity;
-import com.example.gs.gsmvvm.DataBindingMvvmActivity;
+import com.example.gs.gsmvvm.databinding.DataBindingMvvmActivity;
+import com.example.gs.gsmvvm.livedata.LiveDataMvvmActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button btMVP, btMVVM, btMVI;
+    private Button btMVP, btDataBindingMVVM, btLiveDataMVVM, btMVI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +21,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         btMVP = findViewById(R.id.bt_mvp);
-        btMVVM = findViewById(R.id.bt_mvvm);
+        btDataBindingMVVM = findViewById(R.id.bt_databinding_mvvm);
+        btLiveDataMVVM = findViewById(R.id.bt_live_data_mvvm);
         btMVI = findViewById(R.id.bt_mvi);
 
         btMVP.setOnClickListener(this);
-        btMVVM.setOnClickListener(this);
+        btDataBindingMVVM.setOnClickListener(this);
+        btLiveDataMVVM.setOnClickListener(this);
         btMVI.setOnClickListener(this);
     }
 
@@ -36,8 +39,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 intent = new Intent(MainActivity.this, HomeActivity.class);
                 startActivity(intent);
                 break;
-            case R.id.bt_mvvm:
+            case R.id.bt_databinding_mvvm:
                 intent = new Intent(MainActivity.this, DataBindingMvvmActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.bt_live_data_mvvm:
+                intent = new Intent(MainActivity.this, LiveDataMvvmActivity.class);
                 startActivity(intent);
                 break;
             default:
